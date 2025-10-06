c110460804c0022ef34bb1c026de9873b75356fb
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 07:48:11 +0600

    task-2 done

---
+++ task-2.js
@@ -0,0 +1,11 @@
+
+const number = -5;
+
+if (number > 0) {
+    console.log("Positive Number");
+} else if (number === 0) {
+    console.log("Zero Number");
+} else {
+    console.log("Negative Number");
+}
+// Output: Negative Number
