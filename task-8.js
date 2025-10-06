9d54082b8b77c20104ed55e57e8a86f12c7b58a8
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 08:52:09 +0600

    task-8 done

---
+++ task-8.js
@@ -0,0 +1,13 @@
+
+let mark = 85;
+
+if (mark >= 85) {
+    console.log("Grade A");
+}
+else if (mark >= 65) {
+    console.log("Grade B");
+}
+else {
+    console.log("Grade C");
+}
+// Output: Grade A
