b98cec672f3d7ddcb5e7fbca599c53fcd0199a5e
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 07:39:21 +0600

    task-1 done using ternary operator

--- task-1.js
+++ task-1.js
@@ -1,5 +1,6 @@
-let num = 11;
 
+// using if else
+let num = 7;
 if (num % 2 === 0) {
     console.log("Even");
 }
@@ -7,3 +8,10 @@ else {
     console.log("Odd");
 }
 // Output: Odd
+
+
+// using ternary operator
+let a = 10;
+let result = a % 2 === 0 ? "Even" : "Odd";
+console.log(result);
+// Output: Even
