d0aad17e97e83e7565c67937743cc1a7ee5c0c58
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 14:54:12 +0600

    task-15 done

---
+++ task-15.js
@@ -0,0 +1,14 @@
+
+let CP = 100;
+let SP = 150;
+
+if (SP > CP) {
+    console.log("Profit");
+}
+else if (SP < CP) {
+    console.log("Loss");
+}
+else {
+    console.log("No Profit no Loss");
+}
+// Output: Profit
