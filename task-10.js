eb0cac95769366eea4eacb5d4c3fb66074c4b7da
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 09:13:07 +0600

    task-10 done

---
+++ task-10.js
@@ -0,0 +1,12 @@
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+let input = 'b';
+
+if (vowels.includes(input)) {
+    console.log("Vowels");
+}
+else {
+    console.log("Consonant");
+}
+// Output: Consonant
