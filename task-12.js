8591d41d39353fd2dba1c9b2d4b02d8b1988bc51
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 14:35:26 +0600

    task-12 done

---
+++ task-12.js
@@ -0,0 +1,22 @@
+
+let num1 = 10;
+let num2 = 5;
+
+const operator = '*';
+
+if (operator === '+') {
+    console.log(num1 + num2);
+}
+else if (operator === '-') {
+    console.log(num1 - num2);
+}
+else if (operator === '*') {
+    console.log(num1 * num2);
+}
+else if (operator = '/') {
+    console.log(num1 / num2);
+}
+else {
+    console.log("Invalid operator");
+}
+// Output: 50
