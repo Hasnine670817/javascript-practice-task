fbbb55e8efa5ca79d05aff601f99ade6bb8a6bc9
Hasnine670817 <hasninetorun@gmail.com>
2025-10-06 15:20:11 +0600

    task-20 done

---
+++ task-20.js
@@ -0,0 +1,16 @@
+
+let mark = 100;
+
+if (mark >= 90) {
+    console.log("Excellent");
+}
+else if (mark >= 75) {
+    console.log("Very Good");
+}
+else if (mark >= 50) {
+    console.log("Average");
+}
+else {
+    console.log("Needs Improvement");
+}
+// Output: Excellent
